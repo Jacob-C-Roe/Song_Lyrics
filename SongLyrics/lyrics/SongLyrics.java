5e2646f6fa5f7ebf312168f91e9d71842ff3516d
Jacob-C-Roe <143971160+Jacob-C-Roe@users.noreply.github.com>
2023-09-06 13:06:12 -0500

    Add files via upload

---
+++ SongLyrics/lyrics/SongLyrics.java
@@ -0,0 +1,23 @@
+/*
+ * Created By: Jacob Roe
+ * Created On: 9/2/2022
+ * Chapter 1 - Home Practice 1
+ * 
+ * Modification Log
+ * Date		Programmer	Description
+ * 9/2/2022	Jacob Roe	Initial Implementation
+ */
+package lyrics;
+// Establish class Song Lyrics with a class header
+public class SongLyrics {
+// Establish method with a method header
+	public static void main(String[] args) {
+//Write out the body of the method using System.out.println(""); to display the four lines of lyrics from the song I'll Follow You by Imagine Dragons
+		System.out.println("I will follow you way down wherever you may go");
+		System.out.println("I'll follow you way down to your deepest low");
+		System.out.println("I'll always be around wherever life takes you");
+		System.out.println("You know I'll follow you");
+
+	}
+
+}
